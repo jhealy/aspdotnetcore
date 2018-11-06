ef23ef1097b81249ce3d4469e563c0ddae5ef2fe
Joe Healy <josephehealy@hotmail.com>
2018-11-06 11:07:28 -0500

    docviewers initial checkin

---
+++ DocViewersWeb/DocViewersWeb/Pages/GViewIFrame.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DocViewersWeb.Pages
+{
+    public class GViewIFrameModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
